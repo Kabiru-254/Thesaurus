7c8b146f8b46e052decbd7ccaf2a5716b7edec5e
Kabiru <kabiruworld@gmail.com>
2021-04-14 19:45:22 +0300

    Commit 2

--- theSAURUSQ/main/views.py
+++ theSAURUSQ/main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, redirect
+from .models import Question, Answer, Comment
 from django.core.paginator import Paginator
-
-# Create your views here.
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 #Homepage
 def home(request):
@@ -10,7 +12,6 @@ def home(request):
         q=request.GET['q']
         quests=Question.objects.filter(title__icontains=q).order_by('-id')
     else:
-
         quests=Question.objects.all().order_by('-id')
     paginator=Paginator(quests,1)
     page_num=request.GET.get('page',1)
@@ -18,23 +19,47 @@ def home(request):
     return render(request, 'home.html', {'quests':quests})
 
 
-    #Detail
+#Detail
 
-    def detail(request,id):
-        quest=Question.objects.get(pk=id)
-        tags=quest.tags.split(',')
-        answers=Answer.objects.filter(question=quest)
-        answerform=Answerform
-        if request.method=='POST':
-            answerData=AnswerForm(request.POST)
-            if answerData.is_valid():
-                answer=answerData.save(commit=False)
-                answer.question=quest
-                answer.user=request.user
-                answer.save()
-                messages.success(request,'Answer has been submitted.')
-        return render(request, 'detail.html', {
-            'quest':quest,
-            'tags':tags,
-            'answers':answers,
-            'answerform':answerform})
+def detail(request,id):
+    quest=Question.objects.get(pk=id)
+    tags=quest.tags.split(',')
+    answer=Answer.objects.get(question=quest)
+    comments=Comment.objects.filter(answer=answer).order_by('-id')
+    return render(request, 'detail.html', {
+        'quest':quest,
+        'tags':tags,
+        'answer':answer,
+        'comments':comments
+        })
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            message = 'Usename or password is incorrect'
+            form = AuthenticationForm()
+            return render(request, 'login.html', {'form': form, 'message': message})
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form':form})
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == "POST":
+        user = User()
+        form = UserCreationForm()
+        user.username = request.POST.get('username')
+        user.password = request.POST.get('password1')
+        user.save()
+        return redirect('/')
+
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
